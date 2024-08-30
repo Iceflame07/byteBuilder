cffe894f82a63cd2d8509850ab060d77801c89d6
Iceflame07 <Chibizz007@gmail.com>
2024-08-30 07:56:55 +0100

    my second task commit

---
+++ CodeWork/ByteBuilderAssignment/multiplication.py
@@ -0,0 +1,10 @@
+'''
+Prompt the computer to create a program of a given number and save
+Prompt the computer to create a multiplication table and save
+Prompt the computer to enter 1 to 10 a given multiplication and save
+'''
+
+number = int(input("Enter integer: "))
+
+for i in range(1, 11):
+	print(number, " x ", i, " = ", number * i) 
--- CodeWork/ByteBuilderAssignment/nokia3310.py
+++ CodeWork/ByteBuilderAssignment/nokia3310.py
@@ -1,4 +1,4 @@
-menu = """
+menu ="""
 
 1->Phonebook
 2->Messages
@@ -13,13 +13,14 @@ menu = """
 11->Clock
 12->Profiles
 13->SIM services
-"""
+""";
+
 option = int(input(menu))
 
 if option == 1:
 	print("phonebook")
 
-	phonebook_menu = """
+	phonebook_menu ="""
 	1.search
 	2.service Nos.
 	3.add Name
@@ -30,7 +31,7 @@ if option == 1:
 	8.options
 	9->speed dials
 	10->voice tags
-	"""
+	""";
 
 	phonebook_option = int(input(phonebook_menu))
 	if phonebook_option == 1:
@@ -58,10 +59,11 @@ if option == 1:
 	
 	print("Option")
 
-		option_menu = """ 
+	option_menu =""" 
 		1->Type of view
 		2->Memory status
-		"""
+		""";
+
 	phonebook_option = int(input(option_menu))
 	if phonebook_option == 1:
 		print("type_of_view")
@@ -73,7 +75,7 @@ if option == 1:
 elif option == 2:
 	print("Messages")
 
-	message_menu = """
+	message_menu ="""
 	1->Write messages		
 	2->Inbox
 	3->Outbox
@@ -84,7 +86,7 @@ elif option == 2:
 	8->Info service
 	9->Voice mailbox number
 	10->Service command editor
-	"""
+	""";
 	
 	messages_option = int(input(messages_menu))
 	if messages_option == 1:
@@ -112,41 +114,41 @@ elif option == 2:
 
 	print("Set_1")
 
-		set_1_menu = """
+	set_1_menu ="""
 		1->Message center service
 		2->Message sent as
 		3->Message validity
-		"""
+		""";
 
 	set_1_option = int(input(set_1_menu))
-		if set_1_option == 1:
+	if set_1_option == 1:
 			print("message_center_number")
-		if set_1_option == 2:
+	if set_1_option == 2:
 			print("message_sent_as")
-		if set_1_option == 3:
+	if set_1_option == 3:
 			print("message_validity")
 
 	print("Common")
 
-	common_menu = """
+	common_menu ="""
 		1->Delivery report
 		2->Reply via same centre
 		3->Character support
-		"""
+		""";
 	
 	common_option = int(input(common_menu))
-		if set_1_option == 1:
+	if set_1_option == 1:
 			print("delivery_report")
-		if set_1_option == 2:
+	if set_1_option == 2:
 			print("reply_via_same_center")
-		if set_1_option == 3:
+	if set_1_option == 3:
 			print("character_support")
 
 elif option == 3:
 	print("Chat")
 
-	chat_menu = """
-	"""
+	chat_menu ="""
+	""";
 
 	chat_option = int(input(chat_menu))
 	if chat_option == 1:
@@ -155,7 +157,7 @@ elif option == 3:
 elif option == 4:
 	print("Call register")
 
-	call_register_menu = """
+	call_register_menu ="""
 	1->Missed calls		
 	2->Received calls
 	3->Dialed calls
@@ -164,7 +166,7 @@ elif option == 4:
 	6->Show call cost
 	7->Call cost settings
 	8->Prepaid credit
-	"""
+	""";
 
 	call_register_option = int(input(call_register_menu))
 	if call_register_option == 1:
@@ -178,50 +180,50 @@ elif option == 4:
 	if call_register_option == 5:
 		print("show_call_duration")
 
-	print("Show_call_duration")
+	print("Show call duration")
 
-		5.show_call_duration menu = """
-		1->Last call duration
-		2->All calls duration
-		3->Received calls duration
-		4->Dialed calls duration
-		5->Clear timers
-		"""
+	show_call_duration_menu ="""
+	1->Last call duration
+	2->All calls duration
+	3->Received calls duration
+	4->Dialed calls duration
+	5->Clear timers
+	""";
 
 	show_calls_duration_option = int(input(show_calls_duration_menu))
-		if show_calls_duration_option == 1:
+	if show_calls_duration_option == 1:
 			print("last_call_duration")
-		if show_calls_duration_option == 2:
+	if show_calls_duration_option == 2:
 			print("all_calls_duration")
-		if show_calls_duration_option == 3:
+	if show_calls_duration_option == 3:
 			print("receieved_calls_duration")
-		if show_calls_duration_option == 4:
+	if show_calls_duration_option == 4:
 			print("dailed_calls_duration")
-		if show_calls_duration_option == 5:
+	if show_calls_duration_option == 5:
 			print("clear_timers")
 
 	print("Show_call_costs")
 
-		6.show_call_cost menu = """
-		1->Last call cost
-		2->All calls cost
-		3->Clear counter
-		"""
+	show_call_cost_menu ="""
+	1->Last call cost
+	2->All calls cost
+	3->Clear counter
+	""";
 
-		show_calls_cost_option = int(input(show_calls_cost_menu))
-		if show_calls_cost_option == 1:
+	show_calls_cost_option = int(input(show_calls_cost_menu))
+	if show_calls_cost_option == 1:
 			print("last_call_cost")
-		if show_calls_cost_option == 2:
+	if show_calls_cost_option == 2:
 			print("all_calls_cost")
-		if show_calls_cost_option == 3:
+	if show_calls_cost_option == 3:
 			print("clear_counters")
 
 	print("Call_cost_settigs")
 
-		7.call_cost_settings menu = """
+	7->call_cost_settings menu ="""
 		1->Call cost limit
 		2->Show costs in
-		"""
+		""";
 
 	Call_cost_settings_option = int(input(call_cost_settings_menu))
 		if call_cost_settings_option == 1:
@@ -232,7 +234,7 @@ elif option == 4:
 elif option == 5:
 	print("Tones")
 
-	tones_menu = """
+	tones_menu ="""
 	1->Ringing tones
 	2->Ringing volume
 	3->Incoming call alert
@@ -242,7 +244,7 @@ elif option == 5:
 	7->Warning and game tones
 	8->Viberating alert
 	9->Screen saver
-	"""
+	""";
 
 	Tones_option = int(input(tones_menu))
 	if tones_option == 1:
@@ -267,14 +269,14 @@ elif option == 5:
 elif option == 6:
 	print("Settings")
 
-	1.Call_settings menu = """
-	1->Automatic redial
-	2->Speed dialling
-	3->Call waiting option
-	4->Own sending option
-	5->Phone line in use
-	6->Automatic answer
-	"""
+	1->Call_settings menu ="""
+	  1->Automatic redial
+	  2->Speed dialling
+	  3->Call waiting option
+	  4->Own sending option
+	  5->Phone line in use
+	  6->Automatic answer
+	  """;
 
 	call_settigs_option = int(input(call_settings_menu))
 	if tones_option == 1:
@@ -292,14 +294,14 @@ elif option == 6:
 
 	print("Phone_settings")
 
-		phone_settings menu = """
+		phone_settings menu ="""
 		1->Language
 		2->Cell info display
 		3->Welcome note
 		4->Network selection
 		5->Lights
 		6->Confirm SIM service actions
-		"""
+		""";
 
 	phone_settings_option = int(input(phone_settings_menu))
 	if tones_option == 1:
@@ -315,17 +317,17 @@ elif option == 6:
 	if tones_option == 6:
 		print("confirm_SIM_service_actions")
 	
-	3.Security settings
-	print("Security settings")
+	3->Security settings
+	  print("Security settings")
 
-	security_settings menu = """
-	1->Pin code request
-	2->Call barring service
-	3->Fixed dialling
-	4->Closed user group
-	5->Phone security
-	6->Change access codes
-	"""
+	  security_settings menu ="""
+	  1->Pin code request
+	  2->Call barring service
+	  3->Fixed dialling
+	  4->Closed user group
+	  5->Phone security
+	  6->Change access codes
+	  """;
 
 	security_settings_option = int(input(security_settings_menu))
 	if tones_option == 1:
@@ -341,11 +343,11 @@ elif option == 6:
 	if tones_option == 6:
 		print("change_access_codes")
 
-	4.Restore factory settings
-	print("Restore_factory_settings")
+	4->Restore factory settings
+	  print("Restore_factory_settings")
 
-	restore_factory_settings menu = """
-	"""
+	  restore_factory_settings menu ="""
+	  """;
 
        restore_factory_settings_option = int(input(security_settings_menu))
 	if restore_factory_settings_option == 1:
@@ -355,8 +357,8 @@ elif option == 6:
 elif option == 7:
 	print("Call divert")
 
-	call_divert_menu = """
-	"""
+	call_divert_menu ="""
+	""";
 
 	call_divert_option = int(input(call_divert_menu))
 	if call_divert_option == 1:
@@ -366,8 +368,8 @@ elif option == 7:
 elif option == 8:
 	print("Games")
 
-	games_menu = """
-	"""
+	games_menu ="""
+	""";
 
 	games_option = int(input(games_menu))
 	if games_option == 1:
@@ -377,8 +379,8 @@ elif option == 8:
 elif option == 9:
 	print("Calculator")
 
-	calculator_menu = """
-	"""
+	calculator_menu ="""
+	""";
 
 	calculator_option = int(input(calculator_menu))
 	if calculator_option == 1:
@@ -424,8 +426,8 @@ elif option == 11:
 elif option == 12:
 	print("Profiles")
 
-	profiles_menu = """
-	"""
+	profiles_menu ="""
+	""";
 
 	profiles_option = int(input(profles_menu))
 	if profiles_option == 1:
@@ -434,8 +436,8 @@ elif option == 12:
 elif option == 13:
 	print("Sim_services")
 
-	sim_services_menu = """
-	"""
+	sim_services_menu ="""
+	""";
 
 	sim_services_option = int(input(sim_services_menu))
 	if call_divert_option == 1:
