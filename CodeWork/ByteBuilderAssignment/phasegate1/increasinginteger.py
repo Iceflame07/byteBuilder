3125bf93c06fdb38eeef82978ae4f9e1c4618c85
Iceflame07 <Chibizz007@gmail.com>
2024-09-30 12:49:30 +0100

    <sumofintegers.py>

---
+++ CodeWork/ByteBuilderAssignment/phasegate1/increasinginteger.py
@@ -0,0 +1,12 @@
+'''
+prompt the user to input the first number and save
+prompt the user to input the second number and save
+prompt the user to input the third number and save
+prompt the computer to display them in increasing numbers and save
+'''
+
+first_number = int(input("Enter the first number:"))
+
+second_number = int(input("Enter the second number:"))
+
+third_number = int(input("Enter the third number:"))
---
+++ CodeWork/ByteBuilderAssignment/phasegate1/sumofintegers.py
@@ -0,0 +1,10 @@
+'''
+Prompt the computer to read input from 0 - 1000 and save
+prompt the computer to sum all numbers in the integers and save
+prompt the computer to display the result and save
+'''
+
+sum = 0
+for number in range(0, 1000):
+ sum = sum +number
+print(sum)
