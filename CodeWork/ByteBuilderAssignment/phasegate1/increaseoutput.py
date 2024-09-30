7cb04b87391b52a6064123ff81de4bf2d8fe5a77
Iceflame07 <Chibizz007@gmail.com>
2024-09-30 19:45:50 +0100

    <increaseoutput>

--- CodeWork/ByteBuilderAssignment/phasegate1/increaseoutput.py
+++ CodeWork/ByteBuilderAssignment/phasegate1/increaseoutput.py
@@ -5,13 +5,12 @@ prompt the user to input the third number and save
 prompt the computer to display them in increasing numbers and save
 '''
 
-number1 = int(input("Enter the first_number:3"))
-number2 = int(input("Enter the second_number:7"))
-number3 = int(input("Enter the third_number:2"))
+number1 = int(input("Enter the first_number:"))
+number2 = int(input("Enter the second_number:"))
+number3 = int(input("Enter the third_number:"))
 
-total = 0
-for number in number:
- output += number
+total = number1 + number2 + number3
+for number in range(total):
+ total += number
 
-	return output
-print(smallest + average + biggest(2,3,7))
+print(number)
