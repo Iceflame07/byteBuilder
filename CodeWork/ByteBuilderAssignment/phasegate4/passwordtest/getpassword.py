e2b2b5f4289e308e59b57d89dfaa4eb33dc09bbe
Iceflame07 <Chibizz007@gmail.com>
2024-10-21 17:08:52 +0100

    <phasegate4>

---
+++ CodeWork/ByteBuilderAssignment/phasegate4/passwordtest/getpassword.py
@@ -0,0 +1,25 @@
+import unittest
+import getpassword
+
+class Test(unittest.TestCase):
+
+    def test_that_getpassword_function_exist(self):
+	    getpassword("")
+
+    def test_that_digits_has_minimum_length_function(self):
+	    self.assertEqual(getpassword("@Sem&iCh90%B57A$"))
+
+    def test_that_digits_has_lowercase_function(self):
+	    self.assertEqual(getpassword("@Sem&iCh90%B57A$"))
+
+    def test_that_digits_has_uppercase_function(self):
+	    self.assertEqual(getpassword("@Sem&iCh90%B57A$"))
+
+    def test_that_digits_has_symbols_function(self):
+	    self.assertEqual(getpassword("@Sem&iCh90%B57A$"))
+
+    def test_that_digits_are_sixteen_function(self):
+	    self.assertEqual(getpassword("@Sem&iCh90%B57A$"))
+
+    def test_that_digits_are_present_function(self):
+	    self.assertEqual(minimum_length, lowercase, uppercase, digits, symbols, "invalid_input")
