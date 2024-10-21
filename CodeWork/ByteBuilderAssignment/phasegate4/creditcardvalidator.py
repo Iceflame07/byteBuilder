42b5ac59e008ebddee762d275939d8a93ba6148f
Iceflame07 <Chibizz007@gmail.com>
2024-10-21 08:49:38 +0100

    <phasegate4>

---
+++ CodeWork/ByteBuilderAssignment/phasegate4/creditcardvalidator.py
@@ -0,0 +1,48 @@
+'''
+prompt the user to input card number and save
+prompt the computer to check for valid card number and save
+prompt the computer to check if the card starts with {4, 5, 37, 6} and save
+prompt the computer to input valid visa card if it's 4 and save
+prompt the computer to input valid master card if it's 5 and save
+prompt the computer to input valid american express card if it's 37 and save
+prompt the computer to input valid discover card if it's 6 and save
+prompt the computer to input invalid card if it's not any type and save
+prompt the computer to sum the odd and the even numbers together and save
+display the result and save
+'''
+
+def credit_card(validator):
+
+ number = int(input("Enter card number:"))
+odd = {38}:
+even = {37}
+total = {16}:
+count = {0}:
+
+ for count in range(16):
+	print(count)
+
+  if(count % 10)
+   print("totalNumber is valid!"):
+  else:
+    print("Invalid totalNumber,try again!!!"):
+      
+
+def credit_card(get Card) 
+
+ccNumber = {4388576018402626}:
+VisaCard = {4}:
+mastercard = {5}:
+americanexpress = {37}:
+discover = {6};
+
+	if(ccNumber == 4)
+		print("ccNumber is visacard"):
+ 	   elif(ccNumber == 5)
+		print("ccNumber is mastercard"):
+	   elif(ccNumber == 37)
+		print("ccNumber is americanexpress"):
+	   elif(ccNumber == 6)
+		print("ccNumber is Discover Card"):
+	else:
+		print("Invalid ccNumber,try again!!!"):
